e538b148560c6edb1cc549f80447209c8573723e
Mohammadsulthan <84621064+Mohammadsulthan@users.noreply.github.com>
2021-07-04 04:32:39 -0700

    Add files via upload

---
+++ Thre3.java
@@ -0,0 +1,43 @@
+package dd;
+
+import java.util.Date;
+
+public class Thre3 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println(new Date());
+		System.out.println("before strat a class");
+		System.out.println("i told a kala getting a token:.....");
+		
+				Thread n=new Thread(new Runnable(
+						){
+					vaccinationcenter bb;
+					public void run() 
+					{
+						System.out.println("kala stading on the queue:");
+						try
+						{
+							bb.getvacci();
+						}
+						catch(Exception m)
+						{
+							System.out.println(m);
+						}
+						
+					}
+				},"kala");
+				n.start();
+				System.out.println("class strat 7-9 "+new Date());
+	}
+	static class vaccinationcenter
+	{
+		static void getvacci()throws Exception
+		{
+			Thread hh=Thread.currentThread();
+			String name=hh.getName();
+			Thread.sleep(5000);
+			System.out.println(name+" token getting:");
+		}
+	}
+}
